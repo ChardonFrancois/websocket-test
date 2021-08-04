e40f019232d3d4cc7cc1d68f84e20e01803adb76
Chardon François <francoischardon.contact@gmail.com>
2021-08-04 16:55:50 +0200

    start

---
+++ Try-Websocket/websocket/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container">
+      <head>
+        <title>Socket IO chat</title>
+      </head>
+    </div>
+  )
+}
+
+export default App
